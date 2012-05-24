76046af5b2e73514d0b49dbf882ed1a8ba54ab00
anshul1806 <anshul1806@gmail.com>
2012-05-23 19:19:51 -0700

    Added functional Service working version

---
+++ HelloWorldService/HelloWorldService/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService :IHelloService
+    {
+        public string HelloWorld(HelloData msg)
+        {
+            return String.Format("Hello {0} {1}", msg.Fname, msg.Lname);
+        }
+    }
+}
