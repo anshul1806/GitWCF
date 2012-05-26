da76c34ec7d6fa6150335c16a390d452484e432f
anshul1806 <anshul1806@gmail.com>
2012-05-25 21:47:39 -0700

    Created an Eval Service and Tested the service

---
+++ EvalServiceLibrary/EvalServiceLibrary/Eval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EvalServiceLibrary
+{
+   [DataContract]
+    public class Eval
+    {
+        [DataMember]
+        public String Submitter { get; set; }
+        [DataMember]
+        public String comments { get; set; }
+        [DataMember]
+        public DateTime TimeSent { get; set; }
+    }
+}
---
+++ EvalServiceLibrary/EvalServiceLibrary/EvalService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace EvalServiceLibrary
+{
+   [ServiceBehavior(InstanceContextMode=InstanceContextMode.Single)]
+    public class EvalService :IEval
+    {
+        public List<Eval> evals=  new List<Eval>();
+       
+        public void SetEval(Eval eval)
+        {
+            evals.Add(eval);
+        }
+        public List<Eval> GetEval()
+        {
+            return evals;
+        }
+    }
+}
