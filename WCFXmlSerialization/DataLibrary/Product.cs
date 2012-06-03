86b62361ea7ae4465659ae19e51be944553810ce
anshul1806 <anshul1806@gmail.com>
2012-06-03 16:56:24 -0700

    Added data contract demo

--- WCFXmlSerialization/DataLibrary/Product.cs
+++ WCFXmlSerialization/DataLibrary/Product.cs
@@ -7,17 +7,37 @@ using System.Threading.Tasks;
 using System.Runtime.Serialization;
 
 namespace DataLibrary
-{
-    [DataContract]
+{   [KnownType(typeof(ExtendedProduct))] // if we disable this , the extended class won't be able to get serialized with base class reference into serializer. 
+    [DataContract(Name="FashionProduct", Namespace="http://learningWCF.com")]
     public class Product
     {
-        [DataMember]
+        [DataMember(Name="FashionProductName" ,Order=1)]
         public String Name;
-        [DataMember]
+        [DataMember(Name = "FashionProductType", Order =2)]
         public String Type;
-        [DataMember]
+        [DataMember(Name="FupdateTime",Order=3)]
         public DateTime UpdateTime;
-        [DataMember]
+        [DataMember(Name="Remarks",Order=4)]
         public String Comments;
+        /*Filed below marks the field as required where as emitdefault value will ensure that if field value isn't provided, the field doesn't get 
+         serialized. If that's the case, while deserializing the same file, we will get an exception as the filed is required.*/
+        [DataMember(IsRequired=true,EmitDefaultValue=true, Name="RequiredField",Order=5)]
+        public String ProductId;
     }
+
+
+    /*
+     * Main reason for the extended class is show following:
+     * it derives from the base class product. during the serialization, we only provided base class as a type to serialize
+     * and yet still, we passed the Extended product into the serializer. this has two implications.
+     * a) it will fail if the base class isn't told about the class which is serializing itself.(Use knowntype at the base class).
+     * b) after adding a knowntype to the base class we can easily serialize the class, 
+     * even if the reference to the base class was used for the serialization.
+     */
+    [DataContract]
+    public class ExtendedProduct : Product
+    {
+        [DataMember] public String Designer;
+    }
+  
 }
--- WCFXmlSerialization/WCFXmlSerialization/Program.cs
+++ WCFXmlSerialization/WCFXmlSerialization/Program.cs
@@ -14,6 +14,14 @@ namespace WCFXmlSerialization
     {
         static void Main(string[] args)
         {
+           // run this program from console only. Don't run this program from debug menu. 
+            // this program requires varying number of arguments to be passed under different runs.
+            //following are supported usage.
+            /*
+             * WCFXMLSerialization.exe -d(Desirialize the file generated)
+             * WCFXMLSerialization.exe runs the program and generate the product.xml
+             * */
+            String vInfo = FindDataContractVersionToUse(args);
             if (args.Length > 0 && args[0].Equals("-d"))
             {
                 DeserializeMessage();
@@ -22,33 +30,78 @@ namespace WCFXmlSerialization
             {
                 Serialize();
             }
-           
+
+
+
         }
 
+     
         private static void Serialize()
         {
-           Product prd =  new Product(){
-               Name="Healthy Coffe Beans",
-               Comments="Good for cardiac problems and metabolism",
-               Type = "Caps",
-               UpdateTime =  DateTime.Now
-                };
-           using (FileStream fs = new FileStream("Product.xml", FileMode.Create))
-           {
-               DataContractSerializer ds = new DataContractSerializer(typeof(Product));
-               ds.WriteObject(fs, prd);
-           }
+            Product prd = new Product()
+            {
+                Name = "Healthy Coffe Beans",
+                Comments = "Good for cardiac problems and metabolism",
+                Type = "Caps",
+                UpdateTime = DateTime.Now
+            };
+            ExtendedProduct exProd = new ExtendedProduct();
+            exProd.Name = "Fashion Suit";
+            exProd.Comments = "Really Nice Suit";
+            exProd.Type = "Dress";
+            exProd.UpdateTime = DateTime.Now;
+            exProd.Designer = "Manish Lula";
+
+            using (FileStream fs = new FileStream("Product.xml", FileMode.Create))
+            {
+                DataContractSerializer ds = new DataContractSerializer(typeof(Product));
+                ds.WriteObject(fs, exProd);
+            }
+           
         }
 
         private static void DeserializeMessage()
         {
-            DataContractSerializer ds = new DataContractSerializer(typeof(Product));
-            using (FileStream fs = new FileStream("Product.xml", FileMode.Open))
+            String fileName = "Product.xml";
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
             {
+                DataContractSerializer ds = new DataContractSerializer(typeof(Product));
                 Product prd = ds.ReadObject(fs) as Product;
                 Console.WriteLine(String.Format("****Product information****\n" +
                                     "{0} \n {1} \n {2} \n{3}", prd.Name, prd.Comments, prd.Type, prd.UpdateTime));
+                if(prd is ExtendedProduct )
+                {
+                    ExtendedProduct exP = prd as ExtendedProduct;
+                    Console.WriteLine(String.Format("Mr Designer:\t {0}", exP.Designer));
+                }
             }
         }
+
+        private static String GetFileNameBasedOnVersionInfo(String vInfo)
+        {
+            String fileName = String.Empty;
+            if (!String.IsNullOrEmpty(vInfo) && vInfo.Equals("2"))
+            {
+                fileName = "ProductVersion2.xml";
+            }
+            else
+            {
+                fileName = "Product.xml";
+            }
+            return fileName;
+        }
+
+        private static string FindDataContractVersionToUse(string[] args)
+        {
+            if (args.Length == 3 && args[1].Equals("-v"))
+            {
+                // if -v argument is inluded next arguments becomes version of class to be used
+                String versionInfo = args[2];
+                return versionInfo;
+            }
+            return String.Empty;
+        }
+
     }
 }
